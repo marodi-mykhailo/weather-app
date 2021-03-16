0247d0e3470a78ba4bb3b800c874721cfc8d4da3
marodi-mykhailo <mykhailo.marodi@gmail.com>
2021-03-16 19:13:05 +0100

    change to  HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {store} from "./redux/store";
 import {Provider} from "react-redux";
 
 
 ReactDOM.render(
     <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
             <App/>
-        </BrowserRouter>
+        </HashRouter>
     </Provider>,
     document.getElementById('root')
 );
